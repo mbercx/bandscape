061aa8332010f5f297f04e46089c5e07add28ff3
Marnik Bercx <mbercx@gmail.com>
2018-07-19 16:10:48 +0200

    Adapting BandScape object to new brillouin zone methods.

--- bandscape/core.py
+++ bandscape/core.py
@@ -88,12 +88,14 @@ class BandScape(object):
 
         :return:
         """
-        bz = set_up_brillouin(self._out.lattice)
+        neighbors = set_up_neighbors(self._out.lattice)
 
         kpts_bz = []
 
         for k in self.kpoints:
-            kpts_bz.append(return_to_brillouin(k, bz, cartesian=False))
+            kpts_bz.append(return_to_brillouin(k, self._out.lattice,
+                                               neighbors=neighbors,
+                                               cartesian=False))
 
         all_kpts = np.array(kpts_bz[0])
         all_lu_energies = np.array([self.lu_energies[0], ])
@@ -137,7 +139,7 @@ class BandScape(object):
         """
         return cls(Vasprun(filename))
 
-    def find_energy_map(self, kpoint, cartestian=False):
+    def find_energy_map(self, kpoint, cartesian=False):
         """
         Find the minimum energy map for transitions to the lowest unoccupied
         states starting from a chosen initial kpoint in the IBZ.
@@ -148,6 +150,10 @@ class BandScape(object):
         Returns:
 
         """
+        if self._all_kpoints is None:
+            self.reconstruct_bz_kpoints()
+
+        print("setting up index")
 
         kpoint_index = [i for i, x in enumerate(list(self.kpoints)) if
                         np.linalg.norm(x - np.array(kpoint)) < 1e-4]
@@ -164,9 +170,13 @@ class BandScape(object):
 
         else:
 
-            bz = set_up_brillouin(self._out.lattice)
+            print("setting up neighbors")
+
+            neighbors = set_up_neighbors(self._out.lattice)
 
             ho_energy = self.ho_energies[kpoint_index]
+
+            print("setting up q vectors")
 
             q_vectors = self._all_kpoints - np.vstack(
                 len(self._all_kpoints) * [kpoint])
@@ -176,20 +186,30 @@ class BandScape(object):
 
             q_vectors_bz_c = []
 
+            print("mapping q vectors to 1st BZ")
+
             for i, q in enumerate(q_vectors_c):
                 try:
-                    new_q = return_to_brillouin(q, bz, cartesian=True)
+                    new_q = return_to_brillouin(q, self._out.lattice,
+                                                neighbors=neighbors,
+                                                cartesian=True)
                     q_vectors_bz_c.append(new_q)
                 except ValueError:
                     print("Found a kpoint that could not be returned to 1st BZ, "
                           "ignoring...")
                     np.delete(lu_energies, i, axis=0)
 
+            print("done!")
+
+            print("Calculating coordinates in fractional coords.")
+
             q_vectors_bz = np.dot(q_vectors_bz_c, np.linalg.inv(
                 self._out.lattice_rec.matrix))
 
             q_110 = q_vectors_bz[0]
             lu_110 = np.array([lu_energies[0], ])
+
+            print("extracting q vectors in 110 plane.")
 
             # Extract the kpoints in the 110 plane
             for k, energy in zip(q_vectors_bz[1:], lu_energies[1:]):
@@ -337,98 +357,6 @@ class BandScape(object):
 
         plt.show()
 
-
-def plot_map(vasprun_file):
-    out = Vasprun(vasprun_file)
-
-    kpoints = out.actual_kpoints
-    eigenvals = list(out.eigenvalues.values())[0]
-
-    # Find the energies of the lowest unoccupied states
-
-    lu_energies = []
-
-    for kpoint in eigenvals:
-        i = 1
-        while kpoint[i, 1] == 1:
-            i += 1
-        lu_energies.append(kpoint[i, 0])
-
-    bz = set_up_brillouin(out.lattice)
-    kpts_bz = []
-
-    for k in kpoints:
-        kpts_bz.append(return_to_brillouin(k, bz, cartesian=False))
-
-    print("Returned all VASP kpoints to the 1st BZ.")
-
-    all_kpts = np.array(kpts_bz[0])
-    all_lu_energies = np.array([lu_energies[0], ])
-
-    bs = out.get_band_structure()
-
-    # Substract the maximum valence energy
-    lu_energies -= bs.get_vbm()["energy"]
-
-    # Reconstruct all the kpoints with the corresponding energies
-    for k, energy in zip(kpts_bz[1:], lu_energies[1:]):
-        # Get all symmetry equivalent kpoints of the kpoint in the IBZ
-        sym_kpoints = bs.get_sym_eq_kpoints(k)
-
-        # Add the list of kpoints to the total array
-        all_kpts = np.vstack([all_kpts, sym_kpoints])
-
-        # Also add the energy value of the corresponding kpoint #sympoints
-        # times
-        all_lu_energies = np.vstack(
-            [all_lu_energies, np.ones([len(sym_kpoints), 1]) * energy]
-        )
-
-    print("Reconstructed complete 1st BZ.")
-
-    kpts_110 = all_kpts[0]
-    lu_110 = np.array([all_lu_energies[0], ])
-
-    # Extract the kpoints in the 110 plane
-    for k, energy in zip(all_kpts[1:], all_lu_energies[1:]):
-        if abs(k[2]) < 1e-5:
-            kpts_110 = np.vstack([kpts_110, k])
-            lu_110 = np.vstack([lu_110, energy])
-
-    # Set up a new axis system to find suitable coordinates
-    b1 = out.lattice_rec.matrix[0, :]
-    b2 = out.lattice_rec.matrix[1, :]
-
-    vx = b1 - b2
-    vx = vx / np.linalg.norm(vx)
-    vy = b1 + b2
-    vy = vy / np.linalg.norm(vy)
-    vz = np.cross(vx, vy)
-
-    v_mat = np.vstack([vx, vy, vz])
-
-    # Find the coordinates of the kpoints in this new axis system
-    kpts_110_v = np.dot(np.dot(kpts_110, out.lattice_rec.matrix),
-                        np.linalg.inv(v_mat))
-
-    # Interpolate
-    x, y = np.mgrid[np.min(kpts_110_v[:, 0]):np.max(kpts_110_v[:, 0]):0.01,
-           np.min(kpts_110_v[:, 1]):np.max(kpts_110_v[:, 1]):0.01]
-
-    e = interpolate.griddata(kpts_110_v[:, 0:2], lu_110, (x, y),
-                             method='cubic')
-
-    # Plot
-    plt.pcolor(x, y, e[:, :, 0], vmin=np.nanmin(e), vmax=np.nanmax(e),
-               cmap='viridis')
-    cbar = plt.colorbar()
-    cbar.set_label('Energy (eV)', size='x-large')
-
-    plt.show()
-
-    return None
-
-
 def set_up_brillouin(lattice):
     """
 
@@ -457,14 +385,13 @@ def set_up_neighbors(lattice):
     :param lattice:
     :return:
     """
-    rec_lattice = lattice.reciprocal_lattice
     bz_facet_coords = lattice.get_brillouin_zone()
     neighbors = []
 
     for facet in bz_facet_coords:
         # The coordinates are transferred into sites to be able to use the
         # Facet object more easily.
-        neighbors.append(np.mean(facet, axis=0))
+        neighbors.append(np.mean(facet, axis=0)*2)
 
     return neighbors
 
@@ -485,8 +412,6 @@ def is_in_brillouin(kpoint, lattice, neighbors=None, cartesian=True):
     if not neighbors:
         neighbors = set_up_neighbors(lattice)
 
-    pdb.set_trace()
-
     closest_point = np.array([0, 0, 0])
     dist = np.linalg.norm(kpoint)
 
@@ -501,51 +426,32 @@ def is_in_brillouin(kpoint, lattice, neighbors=None, cartesian=True):
     return in_brillouin, closest_point
 
 
-def return_to_brillouin(kpoint, brillouin, cartesian=True,
-                        max_combinations=3):
+def return_to_brillouin(kpoint, lattice, neighbors=None, cartesian=True):
     """
     Return a kpoint to the first Brillouin zone.
+
+    Currently only works for points that are not too far away from the 1st BZ!
 
     :param kpoint: Reciprocal coordinates of the k-point
     :param lattice: Lattice
 
     :return:
     """
-    if is_in_brillouin(kpoint, brillouin, cartesian=cartesian):
+    in_brillouin, point = is_in_brillouin(kpoint, lattice, cartesian=cartesian)
+
+    if in_brillouin:
         return kpoint
     else:
-        rec_lattice = brillouin[0][0].lattice
+        if not cartesian:
+            kpoint = np.dot(kpoint, lattice.reciprocal_lattice.matrix)
+
+        new_kpoint = kpoint - point
 
         if not cartesian:
-            kpoint = np.dot(kpoint, rec_lattice.matrix)
+            new_kpoint = np.dot(new_kpoint, np.linalg.inv(
+                lattice.reciprocal_lattice.matrix))
 
-        # Get all combinations of the reciprocal lattice vectors and the zero
-        # vector, up to the maximum number of combination length
-        combinations = iter.combinations_with_replacement(
-            list(np.vstack([np.array([0, 0, 0]), rec_lattice.matrix,
-                            - rec_lattice.matrix])), max_combinations
-        )
-
-        # Add the vectors together for each combination of reciprocal lattice
-        # vectors
-        test_vectors = [sum(combination) for combination in combinations]
-
-        vector_number = 1  # Ignore the first test vector, i.e. the zero vector
-        # TODO remove all duplicate vectors
-
-        new_kpoint = None
-        in_brillouin = False
-
-        while not in_brillouin and vector_number < len(test_vectors):
-            new_kpoint = kpoint + test_vectors[vector_number]
-            in_brillouin = is_in_brillouin(new_kpoint, brillouin)
-
-            vector_number += 1
-
-        if not cartesian:
-            new_kpoint = np.dot(new_kpoint, np.linalg.inv(rec_lattice.matrix))
-
-        if in_brillouin:
+        if is_in_brillouin(new_kpoint, lattice, neighbors, cartesian):
             return new_kpoint
         else:
             raise ValueError("Could not find corresponding kpoint in 1st BZ.")
